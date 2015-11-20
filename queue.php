1fe63080be31e519bd821967050b79bc6ffb1358
masted <masted311@gmail.com>
2015-11-20 11:49:57 +0300

    Auto-commit on 20.11.2015 11:49:57

--- queue.php
+++ queue.php
@@ -10,4 +10,4 @@ define('WEBROOT_PATH', __DIR__);
 require_once NGN_PATH.'/init/site-cli.php';
 if (file_exists(PROJECT_PATH.'/init.php')) require PROJECT_PATH.'/init.php';
 
-(new ProjectQueueWorker(isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : 1))->run();
+(new ProjectQueueWorker(isset(R::get('options')['n']) ? R::get('options')['n'] : 1))->run();
